886dc82ec4018570b84e759e8d713512c098273c
Maria B-W <maria.bierbaum-williams@codeclan.com>
2018-07-17 20:02:52 +0100

    adds perimeter function

--- homework_practice.rb
+++ homework_practice.rb
@@ -1,3 +1,7 @@
 def triangle_area(length, height)
 return length * height / 2
 end
+
+def rectangle_perimeter(length, height)
+  return 2 * (length + height)
+end
--- homework_tests.rb
+++ homework_tests.rb
@@ -7,4 +7,10 @@ class Functions_Practice < MiniTest::Test
     result = triangle_area(4, 6)
     assert_equal(12, result)
   end
+
+  def test_perimeter()
+    result = rectangle_perimeter(8, 3)
+    assert_equal(22, result)
+  end
+
 end
